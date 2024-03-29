5269a205a354a76203be4d6acf53b09467314d38
Polonessa <logounova.polina@gmailcom>
2024-03-29 19:15:00 +0300

    лабораторная работа 4

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -9,104 +9,80 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 
-
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements TransactionEvents {
-
-
-    ActivityResultLauncher activityResultLauncher;
-
+public class MainActivity extends AppCompatActivity implements TransactionEvents{
 
     private String pin;
-
-    // Used to load the 'fclient' library on application startup.
-    static {
-        System.loadLibrary("fclient");
-        System.loadLibrary("mbedcrypto");
+    @Override
+    public String enterPin(int ptc, String amount) {
+        pin = new String();
+        Intent it = new Intent(MainActivity.this, PinpadActivity.class);
+        it.putExtra("ptc", ptc);
+        it.putExtra("amount", amount);
+        synchronized (MainActivity.this) {
+            activityResultLauncher.launch(it);
+            try {
+                MainActivity.this.wait();
+            } catch (Exception ex) {
+                //todo: log error
+            }
+        }
+        return pin;
     }
 
+    protected String getPageTitle(String html)
+    {
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(html);
+        String p;
+        if (matcher.find())
+            p = matcher.group(1);
+        else
+            p = "Not found";
+        return p;
 
+    }
 
+    protected void testHttpClient()
+    {
+        new Thread(() -> {
+            try {
+                HttpURLConnection uc = (HttpURLConnection)
+                        (new URL("http://192.168.100.83:5000").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() ->
+                {
+                    Toast.makeText(this, title, Toast.LENGTH_LONG).show();
+                });
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        ru.iu3.fclient.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-
-
-
-        // Получение рандомных значений
-        int res = initRng();
-        byte[] v = randomBytes(10);
-
-        // Проверка библиотеки на шифрование
-        byte[] key = new byte[16];
-        byte[] data = "ThisIsMyPassword12345".getBytes();
-
-        byte[] encryptedData = encrypt(key, data);
-        String encryptedString = new String(encryptedData);
-
-        byte[] decryptedData = decrypt(key, encryptedData);
-        String decryptedString = new String(decryptedData);
-
-
-
-        activityResultLauncher  = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult> () {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            Intent data = result.getData();
-                            // обработка результата
-                           // String pin = data.getStringExtra("pin");
-                           // Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
-                            pin = data.getStringExtra("pin");
-                            synchronized (MainActivity.this) {
-                                MainActivity.this.notifyAll();
-                            }
-                        }
-                    }
-
-                });
-
-
-        // Example of a call to a native method
-        //TextView tv = binding.sampleText;
-        //tv.setText(stringFromJNI());
+    public void transactionResult(boolean result) {
+        runOnUiThread(()-> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT);
+        });
     }
-
-
-
-
-    /**
-     * A native method that is implemented by the 'fclient' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-    public static native int initRng();
-    public static native byte[] randomBytes(int no);
-
-    public static native byte[] encrypt(byte[] key, byte[] data);
-
-    public static native byte[] decrypt(byte[] key, byte[] data);
-
-
-    public native boolean transaction(byte[] trd);
-
-
 
     public static byte[] stringToHex(String s)
     {
@@ -122,50 +98,67 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
         return hex;
     }
 
+    static {
+        System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+    }
 
+    private ActivityMainBinding binding;
+    ActivityResultLauncher activityResultLauncher;
+
+    public native boolean transaction(byte[] trd);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int res = initRng();
+        byte[] d_key = randomBytes(16);
+        Random random = new Random();
+        byte[] mas = new byte[20];
+        for (int i = 0; i < mas.length; ++i) {
+            mas[i] = (byte) ((byte) random.nextInt() % 255);
+        }
+        byte[] encrypt_mas = encrypt(d_key, mas);
+        byte[] decrypt_mas = decrypt(d_key, encrypt_mas);
+
+        activityResultLauncher
+                = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            pin = data.getStringExtra("pin");
+                            synchronized (MainActivity.this) {
+                                MainActivity.this.notifyAll();
+                            }
+                        }
+                    }
+                });
+    }
 
     public void onButtonClick(View v)
     {
-
-        new Thread(()-> {
-            try {
-                byte[] trd = stringToHex("9F0206000000000100");
-                boolean ok = transaction(trd);
-
-            } catch (Exception ex) {
-                // todo: log error
-            }
-        }).start();
-
-
-
+        testHttpClient();
+//        new Thread(()-> {
+//            try {
+//                byte[] trd = stringToHex("9F0206000000000100");
+//                transaction(trd);
+//
+//            } catch (Exception ex) {
+//                // todo: log error
+//            }
+//        }).start();
     }
-
-    @Override
-    public String enterPin(int ptc, String amount) {
-        pin  = new String();
-        Intent it = new Intent(MainActivity.this, PinpadActivity.class);
-        it.putExtra("ptc", ptc);
-        it.putExtra("amount", amount);
-        synchronized (MainActivity.this) {
-            activityResultLauncher.launch(it);
-            try {
-                MainActivity.this.wait();
-            } catch (Exception ex) {
-                //todo: log error
-            }
-        }
-
-        return pin;
-    }
-
-
-
-    @Override
-    public void transactionResult(boolean result) {
-        runOnUiThread(()-> {
-            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
-        });
-    }
-
+    public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
